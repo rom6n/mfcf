66f61abdfdf65a1c69651207661a2c4a984a0be8
Roman <roman.mainn@gmail.com>
2025-05-26 14:55:06 +0500

    back

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
 import { fromNano } from "@ton/core";
-import WebApp from "@twa-dev/sdk";
 
 function App() {
   const {
     contract_address,
     counter,
-    //recent_sender,
-    //owner,
+    recent_sender,
+    owner,
     contract_balance,
     sendIncrement,
     widthdrawCoins,
@@ -18,74 +17,74 @@ function App() {
   } = useMainContract();
   const { connected } = useTonConnect();
 
-  const showAlert = () => {
-    WebApp.showAlert("Hey there!");
-  };
-
   return (
     <div>
-      <div>
-        <TonConnectButton />
-      </div>
-      <div>
-        <div className='Card'>
-          <b>{WebApp.platform}</b>
-          <b>Our contract Address</b>
-          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
-          <b>Our contract Balance</b>
+      <TonConnectButton />
+
+      <div className="container">
+        <div>
+          <h3>Contract Data:</h3>
+          <b>Our contract Address:</b>
+          <div className="Hint">{contract_address?.slice(0, 5)+"..."+contract_address?.slice(20, )}</div>
+          <hr />
+
+          <b>Contract Owner:</b>
+          <div className="Hint">{owner?.toString().slice(0, 5)+"..."+owner?.toString().slice(20, )}</div> 
+          <hr />
+
           {contract_balance && (
-            <div className='Hint'>{fromNano(contract_balance)}</div>
+            <>
+              <b>Contract Balance:</b>
+              <div className="Hint">{fromNano(contract_balance)  ?? "Loading..."}</div>
+              <hr />
+            </>
+          )}
+
+          {recent_sender && (
+            <>
+              <b>Recent sender:</b>
+              <div className="Hint">{recent_sender?.toString().slice(0, 5)+"..."+recent_sender?.toString().slice(20, )}</div>
+              <hr />
+            </>
+          )}
+
+          <>
+            <b>Counter Value:</b>
+            <div>{counter ?? "Loading..."}</div>
+          </>
+        </div>
+        <div>
+          <h3>Contract actions: </h3>
+          {connected ? (
+            <>
+              <p>Increment contract balance by 1 TON, with 0.05 TON as a comission</p>
+              <button onClick={sendIncrement}>Increment</button>
+              <hr />
+
+              <p>Withdraw the TON from contract</p>
+              <button onClick={widthdrawCoins}>Withdraw 1 TON</button>
+              <hr />
+              
+              <p>Withdraw the TON from contract</p>
+              <button onClick={sendDeposit}>Deposit 1 TON</button>
+              <hr />
+            </>
+          ) : (
+            <p>Connect wallet to start action</p>
           )}
         </div>
-
-        <div className='Card'>
-          <b>Counter Value</b>
-          <div>{counter ?? "Loading..."}</div>
+        <div>
+          <a
+            href="https://testnet.tonscan.org/address/EQBnodt3mWVa3p3hKj4f_5f6Sx2XSz6ihqrzlCRpsY97SItk"
+            target="_blank"
+          >
+            explorer
+          </a>
+          <br />
+          <a href="https://github.com/rom6n" target="_blank"> 
+            github
+          </a>
         </div>
-
-        <a
-          onClick={() => {
-            showAlert();
-          }}
-        >
-          Show Alert!
-        </a>
-
-        <br />
-
-        {connected && (
-          <a
-            onClick={() => {
-              sendIncrement();
-            }}
-          >
-            Increment by 5
-          </a>
-        )}
-
-        <br />
-
-        {connected && (
-          <a
-            onClick={() => {
-              sendDeposit();
-            }}
-          >
-            Request deposit of 1 TON
-          </a>
-        )}
-
-        <br />
-
-        {connected && (
-          <a
-            onClick={() => {
-              widthdrawCoins();
-            }}
-          >
-            Request 0.7 TON withdrawal
-          </a>
-        )}
       </div>
     </div>
   );
